2b77f55d433292a191c0f6c042e7da2e122bb082
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 22:01:28 +0700

    Update peringkas-artikel-streamlit_app.py

--- peringkas-artikel-streamlit_app.py
+++ peringkas-artikel-streamlit_app.py
@@ -11,7 +11,6 @@ st.header('Selamat Datang di Aplikasi Ringkas.ID', divider='rainbow')
 st.title('Solusi Meringkas Cepat, Tepat, dan Akurat')
 
 # Input URL dari pengguna
-summarizer = pipeline("summarization")
 url = st.text_input('Masukkan URL artikel yang ingin diringkas:')
 if st.button('Ringkas'):
     # Mengambil teks dari URL
